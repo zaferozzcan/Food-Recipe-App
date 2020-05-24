14aa1b69a0504c9c71f393499e8bc96cc339aa81
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-05-24 15:56:47 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Search Your Food</h1>
     </div>
   );
 }
