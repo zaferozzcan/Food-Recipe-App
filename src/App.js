3f207dbd40cab1b47ea5d2425911d0908aa9fcca
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-05-25 13:23:12 -0400

    Add get respond from the api to get the data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
+import axios from "axios";
 
 export default function App() {
+  const APP_ID = "";
+  const APP_KEY = "";
+  const url =
+    "https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}";
+
+  const getData = async () => {
+    const result = await axios.get(url);
+    console.log(result);
+  };
+
   return (
     <div className="App">
       <h1>Search Your Food</h1>
