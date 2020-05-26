005a95713f6cfc9f7fb28ab1e3f08204875140a7
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-05-26 15:02:07 -0400

    Add form and css

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Axios from "axios";
+
+export default function App() {
+
+  const [recipes, setRecipes] = useState()
+
+  var APP_ID = "acec7a02";
+  var APP_KEY = "dce40b8cc0766f46226e5d82c61ffb62";
+  const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const getData = async () => {
+    const result = await Axios.get(url);
+    console.log(result);
+  };
+
+  return (
+    <div className="App">
+      <h1 onClick={getData}>Food Searching App</h1>
+      <form className="search-form">
+        <input
+          type="text"
+          name="query"
+          autoComplete="off"
+          placeholder="Search Food"
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <div className="recipes" />
+    </div>
+  );
+}
